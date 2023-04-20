7b58e8ac2ed576e8fd920c971ab19667f2e638c9
Vinc3nt <88191487+Vincent4440@users.noreply.github.com>
2023-04-20 20:23:56 +0530

    munch: p404-ify

--- device.mk
+++ device.mk
@@ -1,7 +1,17 @@
 #
-# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2023 Project 404
 #
-# SPDX-License-Identifier: Apache-2.0
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 #
 
 # A/B
@@ -152,6 +162,7 @@ PRODUCT_SYSTEM_PROPERTIES += \
 PRODUCT_VENDOR_PROPERTIES += \
     debug.sf.enable_hwc_vds=1 \
     persist.sys.sf.native_mode=258 \
+    debug.sf.frame_rate_multiple_threshold=60 \
     ro.surface_flinger.set_idle_timer_ms=1000 \
     ro.surface_flinger.set_touch_timer_ms=1000 \
     ro.surface_flinger.set_display_power_timer_ms=1000 \

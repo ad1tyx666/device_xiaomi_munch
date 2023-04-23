44773aa51dcb8493138163540949fb146b18cf2f
ExactExampl <64069095+ExactExampl@users.noreply.github.com>
2023-04-23 17:19:08 +0530

    munch: Enable ro.hwui.render_ahead * Rendering ahead makes some animations handling better (especially QS pulldown on a13 QPR2)

--- device.mk
+++ device.mk
@@ -166,6 +166,7 @@ PRODUCT_VENDOR_PROPERTIES += \
     debug.sf.enable_hwc_vds=1 \
     persist.sys.sf.native_mode=258 \
     debug.sf.frame_rate_multiple_threshold=60 \
+    ro.hwui.render_ahead=20 \
     ro.surface_flinger.set_idle_timer_ms=1000 \
     ro.surface_flinger.set_touch_timer_ms=1000 \
     ro.surface_flinger.set_display_power_timer_ms=1000 \
